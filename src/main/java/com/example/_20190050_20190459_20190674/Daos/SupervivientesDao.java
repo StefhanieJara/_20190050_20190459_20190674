80005e297574152db07085c71d4dc01c4d51544a
Miguel <a20190050@pucp.edu.pe>
2022-06-21 02:05:51 -0500

    comitear

--- src/main/java/com/example/_20190050_20190459_20190674/Daos/SupervivientesDao.java
+++ src/main/java/com/example/_20190050_20190459_20190674/Daos/SupervivientesDao.java
@@ -1,13 +1,78 @@
 package com.example._20190050_20190459_20190674.Daos;
 
+import com.example._20190050_20190459_20190674.Beans.Humano;
 import com.example._20190050_20190459_20190674.Beans.Superviviente;
 
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class SupervivientesDao extends DaoBase {
 
+    public ArrayList<Superviviente> obtenerLista() {
 
+        ArrayList<Superviviente> listasupervivientes = new ArrayList<>();
+
+
+        try (Connection conn = this.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("select h.idHumano, h.nombre_apellido, h.sexo, h.peso, h.fuerza, hum.nombre_apellido\n" +
+                     "from Humanos h\n" +
+                     "inner join Humanos hum on h.idHumano_pareja = hum.idHumano;");) {
+            while (rs.next()) {
+                Superviviente superviviente = new Superviviente();
+                superviviente.setId(rs.getString(1));
+                superviviente.setNombre_apellido(rs.getString(2));
+                superviviente.setSexo(rs.getString(3));
+                superviviente.setPeso(rs.getFloat(4));
+                superviviente.setFuerza(rs.getFloat(5));
+                superviviente.setPareja(rs.getString(6));
+
+                listasupervivientes.add(superviviente);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo realizar la busqueda");
+        }
+        return listasupervivientes;
+    }
+
+    public void actualizarSuperviviente(Superviviente superviviente) {
+
+        String sql = "UPDATE humanos SET nombre_apellido = ?, fuerza = ?, peso = ?, idHumano_pareja = ? WHERE (idHumano = ?)";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setString(1, superviviente.getNombre_apellido());
+            pstmt.setFloat(2, superviviente.getFuerza());
+            pstmt.setFloat(3, superviviente.getPeso());
+            pstmt.setString(4,superviviente.getPareja());
+            pstmt.setString(5,superviviente.getId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void crearSupervivencia(String id,String nombre_apellido,String sexo,int id_rol,float fuerza, float peso) {
+
+        String sql = "INSERT INTO humanos (idHumano, nombre_apellido, Sexo, Rol_idRol, fuerza, peso) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+
+            pstmt.setString(1, id);
+            pstmt.setString(2, nombre_apellido);
+            pstmt.setString(3, sexo);
+            pstmt.setInt(4, id_rol);
+            pstmt.setFloat(5,fuerza);
+            pstmt.setFloat(6, peso);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
