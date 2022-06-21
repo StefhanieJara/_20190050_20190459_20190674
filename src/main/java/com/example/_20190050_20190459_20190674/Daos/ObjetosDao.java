c281553a8c5707c5625ca4e1e862c527cf239b40
Miguel <a20190050@pucp.edu.pe>
2022-06-21 18:19:51 -0500

    objetos

--- src/main/java/com/example/_20190050_20190459_20190674/Daos/ObjetosDao.java
+++ src/main/java/com/example/_20190050_20190459_20190674/Daos/ObjetosDao.java
@@ -1,12 +1,10 @@
 package com.example._20190050_20190459_20190674.Daos;
 
 import com.example._20190050_20190459_20190674.Beans.Objetos;
+import com.example._20190050_20190459_20190674.Beans.Superviviente;
 import com.example._20190050_20190459_20190674.Beans.Zombie;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ObjetosDao extends DaoBase {
@@ -58,6 +56,76 @@ public class ObjetosDao extends DaoBase {
         }
 
         return listaObjetos;
+    }
+
+    public void crearObjeto_normal(String nombre, Float peso) {
+
+        String sql = "INSERT INTO objetos (`nombre`, `peso`) VALUES (?, ?);";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setString(1, nombre);
+            pstmt.setFloat(2,peso);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void crearVacuna(String idEfectividad) {
+
+        String sql = "INSERT INTO vacuna (`idEfectividad`) VALUES (?)";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setString(1, idEfectividad);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void crearVacuna_objeto(String nombre, float peso, String idVacuna) {
+
+        String sql = "INSERT INTO objetos (`nombre`, `peso`, `Vacuna_idVacuna`) VALUES (?, ?, ?)";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setString(1, nombre);
+            pstmt.setFloat(2,peso);
+            pstmt.setString(3,idVacuna);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void actualizarObjetos(Objetos objetos) {
+
+        String sql = "UPDATE objetos SET `peso` = ? WHERE (`idObjetos` = ?);";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setFloat(1, objetos.getPeso());
+            pstmt.setInt(2, objetos.getId_objeto());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void actualizarEfictividad_vacuna(Objetos objetos) {
+
+        String sql = "UPDATE vacuna SET `idEfectividad` = ? WHERE (`idVacuna` = ?);";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setInt(1, objetos.getId_efectividad());
+            pstmt.setFloat(2, objetos.getId_vacuna());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
