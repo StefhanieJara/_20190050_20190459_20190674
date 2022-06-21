ab14e21daf6f95cd3ee7c5e093690adfdf82852e
DiegoZuasnabar <a20190459@pucp.edu.pe>
2022-06-20 20:10:30 -0500

    lala

--- src/main/java/com/example/_20190050_20190459_20190674/Daos/HumanoDao.java
+++ src/main/java/com/example/_20190050_20190459_20190674/Daos/HumanoDao.java
@@ -1,4 +1,34 @@
 package com.example._20190050_20190459_20190674.Daos;
 
-public class HumanoDao {
+import com.example._20190050_20190459_20190674.Beans.Humano;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class HumanoDao extends DaoBase{
+    public ArrayList<Humano> obtenerPerfil() {
+
+        ArrayList<Humano> listaHumano = new ArrayList<>();
+
+
+        try (Connection conn = this.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM ;");) {
+            while (rs.next()) {
+                Humano humano = new Humano();
+
+                listaHumano.add(humano);
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo realizar la busqueda");
+        }
+        return listaHumano;
+    }
+
+
 }
