5568c9f07207951e1cb1b31e4b2f3ee85e68b6dc
Miguel <a20190050@pucp.edu.pe>
2022-06-20 19:23:41 -0500

    .

---
+++ src/main/java/com/example/_20190050_20190459_20190674/Beans/Humano.java
@@ -0,0 +1,5 @@
+package com.example._20190050_20190459_20190674.Beans;
+
+public class Humano {
+    private int nombre_apellido;
+}
---
+++ src/main/java/com/example/_20190050_20190459_20190674/Daos/HumanoDao.java
@@ -0,0 +1,4 @@
+package com.example._20190050_20190459_20190674.Daos;
+
+public class HumanoDao {
+}
